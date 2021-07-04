133a16d53aa830709f62906950429558dd4263ff
gwanyong <78327961+gwanyong@users.noreply.github.com>
2021-07-04 14:03:28 +0900

    instaclone-2nd version

---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginGwanyong from "./pages/gwanyong/Login/Login";
+
+import MainGwanyong from "./pages/gwanyong/Main/Main";
+
+class Routes extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={LoginGwanyong} />
+                    <Route
+                        exact
+                        path="/MainGwanyong"
+                        component={MainGwanyong}
+                    />
+                </Switch>
+            </Router>
+        );
+    }
+}
+export default Routes;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
+
+import "./styles/styles/reset.scss";
+import "./styles/styles/common.scss";
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
